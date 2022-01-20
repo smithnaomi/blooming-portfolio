a1a94891b3e87df77f933d7d3947501e84cb61c3
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-19 23:09:50 -0500

    Added About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-// import 'bootstrap/dist/css/bootstrap.css';
 
-function About() {
+export default function About() {
   return (
-    <section>
-      <Container>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
-    </section>
+    <>
+      <h1>
+        <p>About</p>
+        <p>Me</p>
+      </h1>
+      <p className="aboutme">
+        I am a Full Stack Web Developer; graduating from a full stack bootcamp
+        through University of Chapel Hill. Graduating with knowledge in
+        JavaScript, HTML, CSS, Bootstrap, MySQL, React, etc. Currently working
+        as an veterinary technician at an emergency animal hospital. Curiosity,
+        desire to learn, self-motivation and creativity drove me to change my
+        career path. Working in the veterinary medicine I have acquired many
+        skills that transition into tech. The skills I have acquired are but not
+        limited to critical thinking, love for research, patience, collaboration
+        and communication. The goal is to find a company that can continue to
+        nurture these skills, be apart of a company that will continue to let me
+        grow. As well as, being able to find ways to add to a company and team.
+        Reach out, I looked forward to speaking to you.
+      </p>
+    </>
   );
 }
-
-export default About;
