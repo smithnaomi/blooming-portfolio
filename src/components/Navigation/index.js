1b74daf8bfa639402c327ade3e56eca640c4505e
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-28 20:09:17 -0500

    changed import

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,36 +1,36 @@
-import React from "react";
+// import React from "react";
 
-export default function Navigation({ currentPage, handlePageChange }) {
-  return (
-    <>
-      <a
-        href="#aboutme"
-        onClick={() => handlePageChange("About")}
-        className={currentPage === "About" ? "nav active" : "nav"}
-      >
-        About Me
-      </a>
-      <a
-        href="#Projects"
-        onClick={() => handlePageChange("Projects")}
-        className={currentPage === "Projects" ? "nav active" : "nav"}
-      >
-        Projects
-      </a>
-      <a
-        href="#contactForm"
-        onClick={() => handlePageChange("Contact")}
-        className={currentPage === "Contact" ? "nav active" : "nav"}
-      >
-        Contact
-      </a>
-      <a
-        href="#Resume"
-        onClick={() => handlePageChange("Resume")}
-        className={currentPage === "Resume" ? "nav active" : "nav"}
-      >
-        Resume
-      </a>
-    </>
-  );
-}
+// export default function Navigation({ currentPage, handlePageChange }) {
+//   return (
+//     <>
+//       <a
+//         href="#aboutme"
+//         onClick={() => handlePageChange("About")}
+//         className={currentPage === "About" ? "nav active" : "nav"}
+//       >
+//         About Me
+//       </a>
+//       <a
+//         href="#Projects"
+//         onClick={() => handlePageChange("Projects")}
+//         className={currentPage === "Projects" ? "nav active" : "nav"}
+//       >
+//         Projects
+//       </a>
+//       <a
+//         href="#contactForm"
+//         onClick={() => handlePageChange("Contact")}
+//         className={currentPage === "Contact" ? "nav active" : "nav"}
+//       >
+//         Contact
+//       </a>
+//       <a
+//         href="#Resume"
+//         onClick={() => handlePageChange("Resume")}
+//         className={currentPage === "Resume" ? "nav active" : "nav"}
+//       >
+//         Resume
+//       </a>
+//     </>
+//   );
+// }
