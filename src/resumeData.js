e743738332d5f1e9fa418bc3786985af5cba3e1b
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-31 21:22:28 -0500

    added links

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,46 +1,42 @@
 let resumeData = {
-  imagebaseurl: "https://github.com/smithnaomi/",
+  imagebaseurl: "https://smithnaomi.github.io/Portfolio/",
   name: "Naomi Smith",
   role: "Full Stack Developer",
-  linkedinId: "https://www.linkedin.com/in/smithnaomi488/",
+  linkedinId: "smithnaomi488",
   twitterid: "thenewCoder",
-  roleDescription:
-    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+
   socialLinks: [
     {
       name: "linkedin",
-      url: "https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+      url: "https://www.linkedin.com/in/smithnaomi488/",
       className: "fa fa-linkedin",
     },
     {
       name: "github",
-      url: "http://github.com/rbhatia46",
+      url: "https://github.com/smithnaomi",
       className: "fa fa-github",
     },
     {
-      name: "skype",
-      url: "http://twitter.com/rbhatia46",
+      name: "twitter",
+      url: "https://twitter.com/thenewCoder",
       className: "fa fa-twitter",
     },
   ],
-  aboutme:
-    "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-  address: "India",
-  website: "https://rbhatia46.github.io",
+  aboutme: " MEMEMEMEMEMEEMMMMEMEM ",
+  address: "Raleigh,NC ",
+  website: "https://github.com/smithnaomi",
   education: [
     {
-      UniversityName: "The LNM Insitute of Information Technology",
-      specialization: "Some specialization",
-      MonthOfPassing: "Aug",
-      YearOfPassing: "2020",
-      Achievements: "Some Achievements",
+      UniversityName: "University of North Carolina at Chapel Hill",
+      specialization: "Full Stack Developer",
+      MonthOfPassing: "January",
+      YearOfPassing: "2022",
     },
     {
-      UniversityName: "Some University",
-      specialization: "Some specialization",
-      MonthOfPassing: "Jan",
-      YearOfPassing: "2018",
-      Achievements: "Some Achievements",
+      UniversityName: "North Carolina A&T State University",
+      specialization: "Animal Science",
+      MonthOfPassing: "May",
+      YearOfPassing: "2017",
     },
   ],
   work: [
@@ -59,7 +55,7 @@ let resumeData = {
       Achievements: "Some Achievements",
     },
   ],
-  skillsDescription: "Your skills here",
+  skillsDescription: "My skills here",
   skills: [
     {
       skillname: "HTML5",
@@ -69,38 +65,6 @@ let resumeData = {
     },
     {
       skillname: "Reactjs",
-    },
-  ],
-  portfolio: [
-    {
-      name: "project1",
-      description: "mobileapp",
-      imgurl: "images/portfolio/phone.jpg",
-    },
-    {
-      name: "project2",
-      description: "mobileapp",
-      imgurl: "images/portfolio/project.jpg",
-    },
-    {
-      name: "project3",
-      description: "mobileapp",
-      imgurl: "images/portfolio/project2.png",
-    },
-    {
-      name: "project4",
-      description: "mobileapp",
-      imgurl: "images/portfolio/phone.jpg",
-    },
-  ],
-  testimonials: [
-    {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
-    },
-    {
-      description: "This is a sample testimonial",
-      name: "Some technical guy",
     },
   ],
 };
