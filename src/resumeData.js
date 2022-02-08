cc3ad9053bf6259a04ff58d9b6fa210dd14e3266
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-02-08 00:53:27 -0500

    added

---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+  imagebaseurl: "https://smithnaomi.github.io/Portfolio/",
+  name: "Naomi Smith",
+  role: "Full Stack Developer",
+  linkedinId: "smithnaomi488",
+  twitterid: "thenewCoder",
+
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/smithnaomi488/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/smithnaomi",
+      className: "fa fa-github",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/thenewCoder",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    " The desire to continue to learn, love for research, creativity and problem solving are a few skills that I bring into tech. I am 26 years old and transitioning from veterinary medicine into tech. My desire is to find a company that will nurture my skills and to find ways to add value to a company.  ",
+  address: "Raleigh,NC ",
+  website: "https://github.com/smithnaomi",
+  education: [
+    {
+      UniversityName: "University of North Carolina at Chapel Hill",
+      specialization: "Full Stack Developer",
+      MonthOfPassing: "January",
+      YearOfPassing: "2022",
+    },
+    {
+      UniversityName: "North Carolina A&T State University",
+      specialization: "Animal Science",
+      MonthOfPassing: "May",
+      YearOfPassing: "2017",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Triangle Veterinary Referral Hospital",
+      specialization: "Veterinary Medical Assistant",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "promoted from patient care assistant",
+    },
+    {
+      CompanyName: "VCA Apex",
+      specialization: "Veterinary Medical Assistant",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2019",
+    },
+  ],
+  skillsDescription: "Proficient in",
+  skills: [
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Node",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Weather Dashboard",
+      description:
+        "User is able to use the application to check the weather in their desired city. Application provides a five day forecast.",
+      imgurl: "images/weather.png",
+      link: "https://smithnaomi.github.io/Weather-Dashboard/",
+      repo: "https://github.com/smithnaomi/Weather-Dashboard/",
+    },
+    {
+      name: "Progressive Budget",
+      description:
+        "An application that helps users track transactions and totals.",
+      imgurl: "images/budget.png",
+      link: "https://murmuring-atoll-81991.herokuapp.com/",
+      repo: "https://github.com/smithnaomi/Progressive-Budget",
+    },
+    {
+      name: "Narrative Treasures",
+      description:
+        "An application used by both Game Runners and players to create manage and interact with a marketplace economy",
+      imgurl: "images/treasures.png",
+      link: "https://narrative-treasures.herokuapp.com/",
+      repo: "https://github.com/jparris3213/Narrative_Treasures",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
