319f09c65f3fe50e897bbe3bab692efc7d67c31e
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-02-19 19:34:15 -0500

    removed contact

--- src/components/About.js
+++ src/components/About.js
@@ -19,14 +19,14 @@ export default class About extends Component {
 
             <div className="row">
               <div className="columns contact-details">
-                <h2>Contact Details</h2>
+                {/* <h2>Contact Details</h2>
                 <p className="address">
                   <span>{resumeData.name}</span>
                   <br />
                   <span>{resumeData.address}</span>
                   <br />
                   <span>{resumeData.website}</span>
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
